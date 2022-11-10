49068a721ff6418cc11ee08670e68d7f2c262096
Rohan Singh <rs7289579@gmail.com>
2022-11-10 06:54:08 +0530

    adding vercel.json

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-app.use(express.json())
-const port = 3000
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+const mongoose = require("mongoose");
+const Post = require("./Models/Post");
+const port = 3000;
 
-let response = fs.readFileSync('./data.json','utf-8')
-let data = JSON.parse(response)
+let response = fs.readFileSync("./data.json", "utf-8");
+let data = JSON.parse(response);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get(('/'),(req,res)=>{
-    res.send('Hello World!')
-})
+app.get("/api/data", (req, res) => {
+  res.send(data);
+});
 
-app.get(('/api/data'),(req,res)=>{
-    res.send(data)
-})
+app.get("/api/data/:age", (req, res) => {
+  let member = data.find((e) => e.age === parseInt(req.params.age));
+  res.send(member);
+});
 
-app.get('/api/data/:age',(req,res)=>{
-    let member = data.find(e => e.age === parseInt(req.params.age)) 
-    res.send(member)
-})
+app.post("/", (req, res) => {
+  let post = new Post({
+    title: req.body.title,
+    desc: req.body.desc,
+  });
 
-app.post('/api/data',(req,res)=>{
-    let newMember = {}
-    newMember.name = req.body.name
-    newMember.age = req.body.age
-    data.push(newMember)
-    fs.writeFileSync('./data.json',JSON.stringify(data))
-    res.send(newMember)
-})
+  if (post) {
+    post.save()
+    console.log(post);
+    res.send(post);
+  }
+});
 
-app.put('/api/data/:age',(req,res)=>{
-    let member = data.find(e => e.age === parseInt(req.params.age)) 
-    if(!member) res.status(400).send('No User Found...')
+app.post("/api/data", (req, res) => {
+  let newMember = {};
+  newMember.name = req.body.name;
+  newMember.age = req.body.age;
+  data.push(newMember);
+  fs.writeFileSync("./data.json", JSON.stringify(data));
+  res.send(newMember);
+});
 
-    member.name = req.body.name
-    member.age = req.body.age
-    fs.writeFileSync('./data.json',JSON.stringify(data))
-    res.send(data)
-})
+app.put("/api/data/:age", (req, res) => {
+  let member = data.find((e) => e.age === parseInt(req.params.age));
+  if (!member) res.status(400).send("No User Found...");
 
-app.delete('/api/data/:age',(req,res) => {
-    let newData = data.filter(e => e.age != parseInt(req.params.age))
-    if(newData == data) res.status(400).send('No User Found...')
+  member.name = req.body.name;
+  member.age = req.body.age;
+  fs.writeFileSync("./data.json", JSON.stringify(data));
+  res.send(data);
+});
 
-    fs.writeFileSync('./data.json',JSON.stringify(newData))
-    res.send(req.params)
-})
+app.delete("/api/data/:age", (req, res) => {
+  let newData = data.filter((e) => e.age != parseInt(req.params.age));
+  if (newData == data) res.status(400).send("No User Found...");
 
-app.listen(port,()=> console.log(`app is listening on port ${port}`))
+  fs.writeFileSync("./data.json", JSON.stringify(newData));
+  res.send(req.params);
+});
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb",
+  () => {
+    console.log("DB connected");
+  }
+);
+
+app.listen(port, () => console.log(`app is listening on port ${port}`));
